5823b407a613ac955a236d065871a8623ffea540
Ece <eceegokdemir@gmail.com>
2023-05-28 22:01:03 +0300

    Fragment Navigation added

--- app/src/main/java/com/ecegokdemir/shoptimise/MainActivity.kt
+++ app/src/main/java/com/ecegokdemir/shoptimise/MainActivity.kt
@@ -15,27 +15,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        showHomeFragment()
 
         binding.bottomNavigation.setOnItemSelectedListener {
             item ->
             when(item.itemId){
                 R.id.menu_home -> {
                     // Home item clicked, show HomeFragment
+                    showHomeFragment()
 
                     true
                 }
                 R.id.menu_chats -> {
                     // Chats item clicked, show ChatsFragment
+                    showChatsFragment()
 
                     true
                 }
                 R.id.menu_my_ads -> {
                     // My Ads item clicked, show MyAdsFragment
+                    showMyAdsFragment()
 
                     true
                 }
                 R.id.menu_account -> {
                     // Account item clicked, show AccountFragment
+                    showAccountFragment()
 
                     true
                 }
@@ -45,4 +50,53 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun showHomeFragment(){
+        //change toolbar textView text to Home
+        binding.toolbarTitleTv.text = "Home"
+        //Show HomeFragment
+        val fragment = HomeFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.FragmentsFL.id,fragment,"HomeFragment")
+        fragmentTransaction.commit()
+    }
+
+    private fun showChatsFragment(){
+        //change toolbar textView text to Chats
+        binding.toolbarTitleTv.text = "Chats"
+        //Show ChatsFragment
+        val fragment = ChatsFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.FragmentsFL.id,fragment,"ChatsFragment")
+        fragmentTransaction.commit()
+    }
+
+    private fun showMyAdsFragment(){
+        //change toolbar textView text to MyAds
+        binding.toolbarTitleTv.text = "MyAds"
+        //Show MyAdsFragment
+        val fragment = MyAdsFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.FragmentsFL.id,fragment,"MyAdsFragment")
+        fragmentTransaction.commit()
+    }
+
+    private fun showAccountFragment(){
+        //change toolbar textView text to Account
+        binding.toolbarTitleTv.text = "Account"
+        //Show AccountFragment
+        val fragment = AccountFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.FragmentsFL.id,fragment,"AccountFragment")
+        fragmentTransaction.commit()
+    }
+
 }
+
+
+
+
+
+
+
+
