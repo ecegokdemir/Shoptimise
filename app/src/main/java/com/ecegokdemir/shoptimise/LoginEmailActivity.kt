bfcd89540b141b76a3de2724b8df3945a11f39a5
Ece <eceegokdemir@gmail.com>
2023-05-30 19:44:23 +0300

    Authentication methods enabled in Firebase and login UI designed

---
+++ app/src/main/java/com/ecegokdemir/shoptimise/LoginEmailActivity.kt
@@ -0,0 +1,11 @@
+package com.ecegokdemir.shoptimise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_email)
+    }
+}
