e56d5a7ae158dd42ef8efb092f3690af3ca44772
Ece <eceegokdemir@gmail.com>
2023-06-21 22:52:09 +0300

    Some adjustments made

--- app/src/main/java/com/ecegokdemir/shoptimise/HomeFragment.kt
+++ app/src/main/java/com/ecegokdemir/shoptimise/HomeFragment.kt
@@ -1,19 +1,50 @@
 package com.ecegokdemir.shoptimise
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ecegokdemir.shoptimise.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private lateinit var mContext: Context
+
+    private companion object{
+        private const val TAG = "HOME_TAG"
+    }
+
+    override fun onAttach(context: Context) {
+        mContext = context
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        binding = FragmentHomeBinding.inflate(LayoutInflater.from(mContext),container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/ecegokdemir/shoptimise/Utils.kt
+++ app/src/main/java/com/ecegokdemir/shoptimise/Utils.kt
@@ -8,6 +8,31 @@ import java.util.*
 // A class that will contain static functions, constants, variables that I will be used in whole application
 object Utils {
 
+    val categories = arrayOf(
+        "All",
+        "Mobiles",
+        "Computer",
+        "Electronics",
+        "Vehicles",
+        "Furniture",
+        "Fashion",
+        "Books",
+        "Sports"
+    )
+
+    val categoryIcons = arrayOf(
+        R.drawable.icon_category_all,
+        R.drawable.icon_category_mobiles,
+        R.drawable.icon_category_computer,
+        R.drawable.icon_electronics,
+        R.drawable.icon_category_vehicles,
+        R.drawable.icon_category_furniture,
+        R.drawable.icon_category_fashion,
+        R.drawable.icon_category_books,
+        R.drawable.icon_category_sports
+
+    )
+
    // A function to show Toast
    fun toast(context: Context,message: String){
        Toast.makeText(context,message,Toast.LENGTH_SHORT).show()
